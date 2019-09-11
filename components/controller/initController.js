0df7795a6436c84c0dbcbc29f1ea595f3740b7bf
Daniel Herrero <danielherrerohernando@gmail.com>
2019-09-11 14:52:50 +0200

    upd listeners

--- components/controller/initController.js
+++ components/controller/initController.js
@@ -1,6 +1,6 @@
 module.exports = () => {
 	const start = async ({ bus }) => {
-		const pubMessage = { id: 'hola' };
+		// const pubMessage = { id: 'hola' };
 		// await bus.publish('particleCreated')(pubMessage);
 
 		const onError = mess => console.error(mess);
@@ -8,11 +8,32 @@ module.exports = () => {
 		const subscribe = bus.subscribe(onError, onStop);
 
 		let i = 1;
-		const processMessage = async message => {
-			console.log('processing active', i, message.body.id);
+		const processCreatedMessage = async message => {
+			console.log('Creation active', i, message.body.payload.ditaMetadata.attributes.id);
 			i++;
 		};
-		subscribe('particleCreated', processMessage);
+		subscribe('particleCreated', processCreatedMessage);
+
+		let v = 1;
+		const processDigestionMessage = async message => {
+			console.log('Digestion active', v, message.body.payload.ditaMetadata.attributes.id);
+			v++;
+		};
+		subscribe('particleDigested', processDigestionMessage);
+
+		let y = 1;
+		const processUpdatedMessage = async message => {
+			console.log('Update active', y, message.body.payload.ditaMetadata.attributes.id);
+			y++;
+		};
+		subscribe('particleUpdated', processUpdatedMessage);
+
+		let x = 1;
+		const processDeletionMessage = async message => {
+			console.log('Deletion active', x, message.body.payload.id);
+			x++;
+		};
+		subscribe('particleDeleted', processDeletionMessage);
 
 		return {};
 	};
--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
 	server: {
 		host: '0.0.0.0',
-		port: 4000,
+		port: 5000,
 	},
 	routes: {
 		admin: {
